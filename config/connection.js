1154febf697ab8f2f09c2da86211c1d9450271f6
Tony Kuang <tonyjkuang@gmail.com>
2018-01-14 10:26:54 +0800

    <burger>

--- config/connection.js
+++ config/connection.js
@@ -10,11 +10,10 @@ if (process.env.JAWSDB_URL) {
 } else {
 	// DB is local on localhost
 	connection = mysql.createConnection({
-		port: 3306,
 		host: 'localhost',
 		user: 'root',
-		password: '',
-		database: 'burgers_db'
+		password: 'hacktheplanet',
+		database: 'todoagain_db'
 	})
 };
 
